a37287c673f4cf0e2b7f275eb5e8ebf8813a6348
Michael <private1384@live.com>
2016-11-14 12:17:29 -0700

    Adds Remaining Functionality
    Adds missing employee functionality, adds: SaleReceipt, PaymentMethod,
    ServiceCharge and TimeCard functionality.  Also removes UnionCorp

--- src/com/company/PaymentMethod.java
+++ src/com/company/PaymentMethod.java
@@ -7,6 +7,38 @@ import java.util.Date;
  */
 public class PaymentMethod {
 
-    Date dt = new Date ();
-    String payOption;
+    private String address;
+    private String accountNumber;
+    private boolean isHeld;
+    
+    public PaymentMethod(){
+    	super();
+    }
+    
+    public PaymentMethod(String accountNumber){
+    	this.setAccountNumber(accountNumber);
+    }
+    public PaymentMethod(String address, boolean isHeld){
+    	this.setAddress(address);
+    	this.setHeld(isHeld);
+    }
+    
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public boolean isHeld() {
+		return isHeld;
+	}
+	public void setHeld(boolean isHeld) {
+		this.isHeld = isHeld;
+	}
 }
--- src/com/company/ServiceCharge.java
+++ src/com/company/ServiceCharge.java
@@ -5,9 +5,41 @@ package com.company;
  */
 public class ServiceCharge {
 
-    int employeeUnionID;
-    int test;
-    double chargeValue;
+    private int employeeUnionID;
+    private int unionId;
+    private double chargeValue;
+    
+    public ServiceCharge(int unionId, int employeeUnionID, double chargeValue){
+    	this.setUnionId(unionId);
+    	this.setChargeValue(chargeValue);
+    	this.setEmployeeUnionID(employeeUnionID);
+    }
 
+	public int getEmployeeUnionID() {
+		return employeeUnionID;
+	}
+
+	public void setEmployeeUnionID(int employeeUnionID) {
+		if(employeeUnionID >= 0)
+			this.employeeUnionID = employeeUnionID;
+	}
+
+	public int getUnionId() {
+		return unionId;
+	}
+
+	public void setUnionId(int unionId) {
+		if(unionId >= 0)
+			this.unionId = unionId;
+	}
+
+	public double getChargeValue() {
+		return chargeValue;
+	}
+
+	public void setChargeValue(double chargeValue) {
+		if(chargeValue >= 0)
+			this.chargeValue = chargeValue;
+	}
 
 }
